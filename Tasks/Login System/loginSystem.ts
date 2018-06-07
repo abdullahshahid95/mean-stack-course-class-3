589161052ebb81c8283706add6bd96175deef06c
Abdullah Shahid <39632413+abdullahshahid95@users.noreply.github.com>
2018-06-07 18:26:22 +0500

    Add files via upload

---
+++ Tasks/Login System/loginSystem.ts
@@ -0,0 +1,23 @@
+function getUserData()
+{
+    var user = {
+                name: prompt("Enter name"),
+                fullName : prompt("Enter full name"),
+                nic : prompt("Enter NIC"),
+                contact : prompt("Enter contact number"),
+                city : prompt("Enter city"),
+                country : prompt("Enter country")
+                };
+
+    
+    //var picture: string = prompt("Enter name");
+
+    alert("Name: " + user.name + "\n\n" + 
+        "Full name: " + user.fullName + "\n\n" + 
+        "NIC: " + user.nic + "\n\n" + 
+        "Contact: " + user.contact + "\n\n" + 
+        "City: " + user.city + "\n\n" + 
+        "Country: " + user.country);
+}
+
+getUserData();
